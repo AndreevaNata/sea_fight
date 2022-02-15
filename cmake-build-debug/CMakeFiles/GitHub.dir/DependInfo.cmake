d937ff137016a8b9006d186795f16f7327ba4f90
anita1k <99723523+anita1k@users.noreply.github.com>
2022-02-15 14:01:21 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/GitHub.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/anitakovalenko/Documents/GitHub/cell_method.cpp" "/Users/anitakovalenko/Documents/GitHub/cmake-build-debug/CMakeFiles/GitHub.dir/cell_method.cpp.o"
+  "/Users/anitakovalenko/Documents/GitHub/main.cpp" "/Users/anitakovalenko/Documents/GitHub/cmake-build-debug/CMakeFiles/GitHub.dir/main.cpp.o"
+  "/Users/anitakovalenko/Documents/GitHub/ship.cpp" "/Users/anitakovalenko/Documents/GitHub/cmake-build-debug/CMakeFiles/GitHub.dir/ship.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
